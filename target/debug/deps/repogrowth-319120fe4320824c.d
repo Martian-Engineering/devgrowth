e6ff579e9bb2cb986999b5d8a2e1fdcd49f75056
Josh Lehman <josh@martian.engineering>
2024-09-28 17:26:35 -0700

    Initial commit

---
+++ target/debug/deps/repogrowth-319120fe4320824c.d
@@ -0,0 +1,5 @@
+/Users/josh/Projects/repogrowth/target/debug/deps/repogrowth-319120fe4320824c: src/main.rs
+
+/Users/josh/Projects/repogrowth/target/debug/deps/repogrowth-319120fe4320824c.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/repogrowth.d
@@ -0,0 +1,1 @@
+/Users/josh/Projects/repogrowth/target/debug/repogrowth: /Users/josh/Projects/repogrowth/src/main.rs
